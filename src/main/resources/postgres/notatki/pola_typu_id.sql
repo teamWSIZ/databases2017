6fb3cf39c61b8e4caa889c1226519c0ae9f0c820
Piotr Marecki <pmarecki.pro@gmail.com>
2017-03-25 11:32:01 +0100

    State at the end of class

--- src/main/resources/postgres/notatki/pola_typu_id.sql
+++ src/main/resources/postgres/notatki/pola_typu_id.sql
@@ -6,20 +6,20 @@
 -- ); --wersja z automatycznym polem typu ID, i z pilnowaniem by tam była wartość
 
 -- INSERT into friends (name, rank) VALUES ('aa', 10);
--- INSERT into friends (fid, name, rank) VALUES (1, 'aa', 10); --drugi `1` --> źle!! tak nie chcemy
+-- INSERT into friends (fid, name, rank) VALUES (1, 'bb', 10); --drugi `1` --> źle!! tak nie chcemy
 -- INSERT into friends (fid, name, rank) VALUES (NULL , 'aa', 10); -- to nie przejdzie -- OK, nie chcemy tam null-i
 -- INSERT into friends (name, rank) VALUES ('aa', null); -- to przejdzie; OK, nie martwimy się tym
 
 -- drop table friends;
 --
 -- Próba 2: pole `fid` będzie z definicji unikalne i indeksowane
--- CREATE table friends (
---   fid SERIAL PRIMARY KEY,
---   name text,
---   rank int
--- ); --dodatkowo pilnuje aby...
+CREATE table friends (
+  fid SERIAL PRIMARY KEY,
+  name text,
+  rank int
+); --dodatkowo pilnuje aby...
 
 -- INSERT into friends (name, rank) VALUES ('aa', 10);
--- INSERT into friends (fid, name, rank) VALUES (1, 'aa', 10); --drugi `1` --> teraz nie przejdzie -- OK!
+INSERT into friends (fid, name, rank) VALUES (1, 'bb', 10); --drugi `1` --> teraz nie przejdzie -- OK!
 -- INSERT into friends (fid, name, rank) VALUES (NULL , 'aa', 10); -- to nie przejdzie -- OK, nie chcemy tam null-i
 -- INSERT into friends (name, rank) VALUES ('aa', null); -- to przejdzie; OK, nie martwimy się tym
