523f7fecc185ef2d103698f80d62b2951a0320ed
Piotr Marecki <pmarecki.pro@gmail.com>
2017-06-10 09:04:23 +0200

    Cwiczenia z zapytaniami

---
+++ src/main/resources/postgres/notatki/cwiczenia_z_zapytaniami.sql
@@ -0,0 +1,28 @@
+set SEARCH_PATH to xxx;
+
+SELECT count(*)/1000 from orders;
+
+
+-- Ile zamówień zrealizował pracownik nr 5 w roku 2015
+SELECT count(*)
+FROM orders
+WHERE employeeid = 5 and orderdate BETWEEN '2015-01-01' AND '2016-12-31';
+
+
+-- Ile razy pracownik nr 5 w 2015 roku wykonał sprzedaż produktu nr 10
+SELECT count(*)
+FROM orders o, orderdetail od
+WHERE od.orderid = o.orderid AND
+      o.employeeid = 5 AND
+      o.orderdate BETWEEN '2015-01-01' AND '2016-12-31' AND
+      od.productid = 10;
+
+-- Ile produktów typu 10 sprzedał pracownik nr 5 w 2015 roku
+SELECT sum(od.quantity)
+FROM orders o, orderdetail od
+WHERE od.orderid = o.orderid AND
+      o.employeeid = 5 AND
+      o.orderdate BETWEEN '2015-01-01' AND '2016-12-31' AND
+      od.productid = 10;
+
+-- Ile było sprzedaży od klientów z argentyny w 2015r ?
